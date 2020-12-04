64d04105a0f3f2960bbf4b0e03abc34b1e800455
Aleks <shenshin@me.com>
2020-12-04 20:08:52 +0100

    Add HackYourTemp exercise

--- week2/homework/hackyourtemperature/lib/server.js
+++ week2/homework/hackyourtemperature/lib/server.js
@@ -1,14 +1,22 @@
 "use strict";
-const express = require('express');
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: undefined }));
 app.get('/', (req, res) => {
-    res.send('hello from backend to frontend!');
+    res.render('index');
+});
+app.post('/weather', (req, res) => {
+    const { cityName } = req.body;
+    res.status(200);
+    res.type('application/json');
+    res.end(JSON.stringify({ cityName }));
 });
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, (err) => {
-    if (err) {
-        console.error(err);
-    }
-    console.log(`Server started on port ${PORT}`);
+app.listen(PORT, () => {
+    process.stdout.write(`Server started on port ${PORT}\n`);
 });
 //# sourceMappingURL=server.js.map
--- week2/homework/hackyourtemperature/src/server.ts
+++ week2/homework/hackyourtemperature/src/server.ts
@@ -1,21 +1,26 @@
 // usage: npm run start
 // (starts auto compile typeScript and auto runs server)
 
-const express = require('express');
-// const path = require('path');
-/* const exphbs = require('express-handlebars'); */
-const app = express();
-/* app.set('view engine', 'handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: false })); */
+import express = require('express');
+import exphbs = require('express-handlebars');
 
-app.get('/', (req: any, res: any) => {
-  res.send('hello from backend to frontend!');
+const app: express.Application = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: undefined }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.post('/weather', (req, res) => {
+  const { cityName } = req.body;
+  res.status(200);
+  res.type('application/json');
+  res.end(JSON.stringify({ cityName }));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, (err: any) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(`Server started on port ${PORT}`);
+app.listen(PORT, () => {
+  process.stdout.write(`Server started on port ${PORT}\n`);
 });
