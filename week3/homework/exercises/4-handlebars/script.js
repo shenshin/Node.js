38a38326b28cb2ed8744aef485015b376d162d9e
Aleks <shenshin@me.com>
2020-12-09 19:29:31 +0100

    Add exercise 4

--- week3/homework/exercises/4-handlebars/script.js
+++ week3/homework/exercises/4-handlebars/script.js
@@ -1,27 +1,26 @@
-
+/* eslint-disable no-console */
 /**
  * 4. Fun with Handlebars
- * 
+ *
  * Write a javascript function that simulates playing the game cards against humanity.
  * The code should choose a subject and a punchline at random,
  * then replace them in a sentece using handlebars.
- * 
+ *
  * Hints:
  * - Check the handlebars npm page for examples and documentation
  */
-
-
-function drawCard() {
-  // YOUR CODE GOES IN HERE
-}
-
-drawCard();
+const handlebars = require('handlebars');
 
 /**
  * Given an array, return an element from it chosen at random
  */
 function getRandomElement(array) {
   // YOUR CODE GOES IN HERE
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 const subjects = [
@@ -43,3 +42,27 @@ const punchlines = [
   'achieve world piece',
   'help people learn programing',
 ];
+
+function drawCard() {
+  // YOUR CODE GOES IN HERE
+  const cardData = {
+    subject: capitalize(getRandomElement(subjects)),
+    punchline: getRandomElement(punchlines),
+  };
+  const card = '{{subject}} is great to {{punchline}}.';
+  const template = handlebars.compile(card);
+  console.log(template(cardData));
+}
+
+drawCard();
+
+// It's easier to achieve the same result as follows, isn't it?
+function drawAnotherCard() {
+  const cardData = {
+    subject: capitalize(getRandomElement(subjects)),
+    punchline: getRandomElement(punchlines),
+  };
+  console.log(`${cardData.subject} is great to ${cardData.punchline}.`);
+}
+
+drawAnotherCard();
